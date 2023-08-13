0b93a868ef20db94adf6ba95dce5d4f772a29b54
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-13 14:55:21 +0300

    add pages folder and lazy import for About and Main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
 	return (
 		<div>
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route
+						path={'/about'}
+						element={<AboutPageAsync />}
+					/>
+					<Route
+						path={'/'}
+						element={<MainPageAsync />}
+					/>
+				</Routes>
+			</Suspense>
 			<Counter />
 		</div>
 	);
