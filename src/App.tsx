d0651af74a6e8b17fd6036fcae4b0afb241d3d1b
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-13 17:49:43 +0300

    add styles and theme folders

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+	const { theme, toogleTheme } = useTheme();
+
 	return (
-		<div>
+		<div className={`App ${theme}`}>
+			<button onClick={toogleTheme}>TOOGLE</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.scss';
+import ThemeProvider from './theme/ThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</BrowserRouter>
 );
