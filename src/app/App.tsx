e3a3e632a30bea51e6adfccbe442472d08e2f2f1
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-16 13:48:46 +0300

    add router provider and config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-
-import './styles/index.scss';
+import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/Router';
+import './styles/index.scss';
 
 const App = () => {
 	const { theme, toogleTheme } = useTheme();
@@ -15,18 +12,7 @@ const App = () => {
 			<button onClick={toogleTheme}>TOOGLE</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route
-						path={'/about'}
-						element={<AboutPage />}
-					/>
-					<Route
-						path={'/'}
-						element={<MainPage />}
-					/>
-				</Routes>
-			</Suspense>
+			<AppRouter />
 		</div>
 	);
 };
