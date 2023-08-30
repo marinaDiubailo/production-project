805845b162e76728ac0eceb3836c543221feefb0
Marina Diubailo <marinadiubailo@gmail.com>
2023-08-30 13:43:31 +0300

    add pre commit husky

---
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { LoginForm } from './LoginForm';
+
+const meta = {
+    title: 'features/LoginForm',
+    component: LoginForm,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof LoginForm>;
+
+export const Light: Story = {
+    args: {},
+};
+export const Dark: Story = {
+    args: {},
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
