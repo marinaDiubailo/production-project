4a9a298d0b721d8962b5e58ebf085e0f2b6521b1
Marina Diubailo <marinadiubailo@gmail.com>
2023-09-27 09:49:37 +0300

    add Copy webpack plugin and modify api.ts

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 // const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'http://production.ru';
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization =
+            localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
