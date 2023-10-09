e61ed4cb39ede2f466c0a262285f6b6fb29dc92a
Marina Diubailo <marinadiubailo@gmail.com>
2023-10-09 16:40:22 +0300

    fix storybook

--- src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,13 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { rest } from 'msw';
 import { Theme } from '@/app/providers/ThemeProvider';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import ArticleRating from './ArticleRating';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta = {
     title: 'features/ArticleRating',
     component: ArticleRating,
     argTypes: {
         backgroundColor: { control: 'color' },
+    },
+    decorators: [
+        StoreDecorator({
+            user: {
+                authData: {
+                    id: '1',
+                },
+            },
+        }),
+    ],
+    args: {
+        articleId: '1',
+    },
+    parameters: {
+        msw: {
+            handlers: [
+                rest.get(
+                    `${__API__}/article-ratings?userId=1&articleId=1`,
+                    (_req, res, ctx) => {
+                        return res(ctx.status(200), ctx.json([{ rate: 4 }]));
+                    }
+                ),
+            ],
+        },
     },
 } as Meta<typeof ArticleRating>;
 
@@ -27,3 +53,33 @@ export const Orange: Story = {
     args: {},
 };
 Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+const withoutRate = {
+    msw: {
+        handlers: [
+            rest.get(
+                `${__API__}/article-ratings?userId=1&articleId=1`,
+                (_req, res, ctx) => {
+                    return res(ctx.status(200), ctx.json([]));
+                }
+            ),
+        ],
+    },
+};
+
+export const LightWithoutRate: Story = {
+    args: {},
+    parameters: withoutRate,
+};
+
+export const DarkWithoutRate: Story = {
+    args: {},
+    parameters: withoutRate,
+};
+DarkWithoutRate.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OrangeWithoutRate: Story = {
+    args: {},
+    parameters: withoutRate,
+};
+OrangeWithoutRate.decorators = [ThemeDecorator(Theme.ORANGE)];
---
+++ src/features/AvatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from '@/app/providers/ThemeProvider';
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import Avatar from '@/shared/assets/tests/storybook.jpg';
+import { UserRole } from '@/entities/User';
+import { AvatarDropdown } from './AvatarDropdown';
+
+const meta = {
+    title: 'features/AvatarDropdown',
+    component: AvatarDropdown,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [
+        StoreDecorator({
+            user: {
+                authData: {
+                    id: '1',
+                    avatar: Avatar,
+                    roles: [UserRole.ADMIN],
+                },
+            },
+        }),
+    ],
+} as Meta<typeof AvatarDropdown>;
+
+export default meta;
+type Story = StoryObj<typeof AvatarDropdown>;
+
+export const Light: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Orange: Story = {
+    args: {},
+};
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
